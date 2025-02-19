a6cbc243adf3cb5b886d109979349b8076ea1f6d
Aqua200 <chinquepapa@gmail.com>
2025-02-18 19:28:02 -0800

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -31,17 +31,17 @@ console.log(`Error : ${e}`)
 global.creador = 'Wa.me/5216631079388'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
 global.asistencia = 'Wa.me/5216631079388'
-global.namechannel = 'ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐁✨𝐭 - ᥙ⍴ძᥲ𝗍ᥱs ❀'
-global.namechannel2 = 'ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐁✨𝐭 - ᥲ᥎іs᥆s ❀"'
-global.namegrupo = '.•♫•♬• 🦋𝐀𝐧𝐢𝐤𝐚-𝐁✨𝐭 -ʙᴏᴛ  •♬•♫•.'
-global.namecomu = '✿:･✧ 𝓨𝓾𝓴𝓲 𝓢𝓾𝓸𝓾 𝓒𝓸𝓶𝓾𝓷𝓲𝓽𝔂 ✧･:✿'
-global.listo = '🍬 *Aquí tienes ฅ^•ﻌ•^ฅ*'
+global.namechannel = 'ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐒𝐭𝐞𝐥𝐥𝐚✨ - 𝐮𝐩𝐝𝐝𝐚𝐭𝐞𝐬 ❀'
+global.namechannel2 = 'ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐒𝐭𝐞𝐥𝐥𝐚✨ - 𝐚𝐯𝐢𝐬𝐨𝐬 ❀"'
+global.namegrupo = '.•♫•♬• 🦋𝐀𝐧𝐢𝐤𝐚-𝐒𝐭𝐞𝐥𝐥𝐚✨ - 𝐁𝐨𝐭  •♬•♫•.'
+global.namecomu = '✿:･✧ 𝐴𝑛𝑖𝑘𝑎 𝑆𝑡𝑒𝑙𝑙𝑎 𝑐𝑜𝑚𝑚𝑢𝑛𝑖𝑡𝑦  ✧･:✿'
+global.listo = '❤️ *Aquí tienes ฅ^•ﻌ•^ฅ*'
 global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg')
 
 //Ids channel
 global.idchannel = '120363392571425662@newsletter'
 global.canalIdM = ["120363393500238364@newsletter", "120363392571425662@newsletter"]
-global.canalNombreM = ["ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐁✨𝐭-mძ - ᥙ⍴ძᥲ𝗍ᥱs ❀", "ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐁✨𝐭-mძ - ᥲ᥎іs᥆s ❀"]
+global.canalNombreM = ["ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐒𝐭𝐞𝐥𝐥𝐚✨ - 𝐮𝐩𝐝𝐝𝐚𝐭𝐞𝐬 ❀", "ᥫ᭡ 🦋𝐀𝐧𝐢𝐤𝐚-𝐒𝐭𝐞𝐥𝐥𝐚✨ - 𝐚𝐯𝐢𝐬𝐨𝐬 ❀"]
 global.channelRD = await getRandomChannel()
 
 //fechas
