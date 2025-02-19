ab4fb129e50e7adb3b779922e21b965ea7726379
Aqua200 <chinquepapa@gmail.com>
2025-02-19 07:29:17 -0800

    Update tools-spamwa.js

--- plugins/tools-spamwa.js
+++ plugins/tools-spamwa.js
@@ -1,24 +1,26 @@
-const handler = async (m, {conn, text}) => {
-const [nomor, pesan, jumlah] = text.split('|');
+const handler = async (m, { conn, text }) => {
+    const [nomor, pesan, jumlah] = text.split('|');
 
-if (!nomor) return conn.reply(m.chat, `${emoji} Por favor, ingrese un número para realizar spam.`, m);
+    if (!nomor) return conn.reply(m.chat, `🌸 Oh no... ¡Parece que olvidaste ingresar un número!`, m);
 
-if (!pesan) return conn.reply(m.chat, `${emoji} Uso Correcto:\n\n> ${emoji2} #spamwa numero|texto|cantidad`, m);
+    if (!pesan) return conn.reply(m.chat, `💌 Umm... creo que olvidaste el mensaje. Inténtalo así:\n\n> ✨ #spamwa numero|texto|cantidad`, m);
 
-if (jumlah && isNaN(jumlah)) return conn.reply(m.chat, `${emoji2} La cantidad deve ser un numero.`, m);
+    if (jumlah && isNaN(jumlah)) return conn.reply(m.chat, `🙈 Oh... la cantidad debe ser un número, ¿sí?`, m);
 
-const fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net';
-const fixedJumlah = jumlah ? jumlah * 1 : 10;
+    const fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net';
+    const fixedJumlah = jumlah ? jumlah * 1 : 10;
 
-if (fixedJumlah > 999) return conn.reply(m.chat, `${emoji3} Minimo 50 Caracteres.`, m);
+    if (fixedJumlah > 999) return conn.reply(m.chat, `😖 Uy... es demasiado grande, ¿puedes intentar con menos?`, m);
 
-await conn.reply(m.chat, `${emoji4} Se envió con éxito el spam.`, m);
-for (let i = fixedJumlah; i > 1; i--) {
-if (i !== 0) conn.reply(fixedNumber, pesan.trim(), null);
-}
+    await conn.reply(m.chat, `🎀 E-esto... ¡El spam se envió con éxito! Espero que no sea molestia...`, m);
+    for (let i = fixedJumlah; i > 1; i--) {
+        if (i !== 0) conn.reply(fixedNumber, pesan.trim(), null);
+    }
 };
-handler.help = ['spamwa <number>|<mesage>|<no of messages>'];
+
+handler.help = ['spamwa <numero>|<mensaje>|<cantidad>'];
 handler.tags = ['tools'];
 handler.command = ['spam', 'spamwa'];
 handler.premium = true;
+
 export default handler;
