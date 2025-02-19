6c642ad35e5911dc6f9dd77279bc3106280d7be3
Aqua200 <chinquepapa@gmail.com>
2025-02-18 18:04:01 -0800

    Update config.js

--- config.js
+++ config.js
@@ -62,8 +62,8 @@ global.etiqueta = 'Neykoor'
 global.moneda = 'Yenes'
 global.welcom1 = 'Edita Con #setwelcome'
 global.welcom2 = 'Edita Con #setbye'
-global.banner = 'https://qu.ax/LDdfg.jpg'
-global.avatar = 'https://qu.ax/yyBkK.jpg'
+global.banner = 'https://qu.ax/gxnkD.jpg'
+global.avatar = 'https://qu.ax/ryZnk.jpg'
 
 //*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
