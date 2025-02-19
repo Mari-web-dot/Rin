7e96039687dcfeddcb9ad1ecc87bfd1a0f707f1c
Aqua200 <chinquepapa@gmail.com>
2025-02-18 23:17:38 -0800

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,34 +1,34 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `✨ *EQUIPO DE AYUDANTES*
-🤖 *Bot:* ${global.botname}
-🌟 *Versión:* ${global.vs}
+    let staff = `✨ *EQUIPO DE AYUDANTES*
+    🤖 *Bot:* ${global.botname}
+    🌟 *Versión:* ${global.vs}
 
-👑 *Propietario:*
+    👑 *Propietario:*
 
-• Destroy
-🤴 *Rol:* Propietario
-📱 *Número:* wa.me/584120346669
+    • Destroy
+    🤴 *Rol:* Propietario
+    📱 *Número:* wa.me/584120346669
 
-🚀  *Colaboradores:*
+    🚀  *Colaboradores:*`
 
-
-await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
-contextInfo: {
-'forwardingScore': 200,
-'isForwarded': false,
-externalAdReply: {
-showAdAttribution: true,
-renderLargerThumbnail: false,
-title: `🥷 Developers 👑`,
-body: `✨ Staff Oficial`,
-mediaType: 1,
-sourceUrl: redes,
-thumbnailUrl: icono
-}}
-}, { mentions: m.sender })
-m.react(emoji)
-
+    await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
+        contextInfo: {
+            'forwardingScore': 200,
+            'isForwarded': false,
+            externalAdReply: {
+                showAdAttribution: true,
+                renderLargerThumbnail: false,
+                title: `Developers 👑`,  // Aquí se reemplaza el emoji
+                body: `✨ Staff Oficial`,
+                mediaType: 1,
+                sourceUrl: redes,
+                thumbnailUrl: icono
+            }
+        }
+    }, { mentions: m.sender })
+    m.react(emoji)
 }
+
 handler.help = ['staff']
 handler.command = ['colaboradores', 'staff']
 handler.register = true
