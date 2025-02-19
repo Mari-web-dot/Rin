9e7b0edca483ef72f796ea698f7e1c42f2c0015f
Aqua200 <chinquepapa@gmail.com>
2025-02-19 12:42:28 -0800

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,19 +1,35 @@
-import speed from 'performance-now'
-import { spawn, exec, execSync } from 'child_process'
+import { exec } from 'child_process';
+import { performance } from 'perf_hooks';
 
-let handler = async (m, { conn }) => {
-         let timestamp = speed();
-         let latensi = speed() - timestamp;
-         exec(`neofetch --stdout`, (error, stdout, stderr) => {
-          let child = stdout.toString("utf-8");
-          let ssd = child.replace(/Memory:/, "Ram:");
+let handler = async (m, { conn, rcanal }) => {
+    // Medir el tiempo antes de ejecutar el comando
+    let startTime = performance.now();
 
-          conn.reply(m.chat, `✰ *¡Pong!*\n> Tiempo ⴵ ${latensi.toFixed(4)}ms`, m);
-            });
+    exec(`neofetch --stdout`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing neofetch: ${error.message}`);
+            return;
+        }
+
+        if (stderr) {
+            console.error(`neofetch stderr: ${stderr}`);
+            return;
+        }
+
+        let child = stdout.toString("utf-8");
+        let ssd = child.replace(/Memory:/, "Ram:");
+
+        // Medir el tiempo después de ejecutar el comando
+        let endTime = performance.now();
+        let latensi = endTime - startTime;
+
+        conn.reply(m.chat, `*Pong* 🏓 ${latensi.toFixed(4)} ms`, m, rcanal);
+    });
 }
-handler.help = ['ping']
-handler.tags = ['info']
-handler.command = ['ping', 'p']
-handler.register = true
 
-export default handler
+handler.help = ['ping'];
+handler.tags = ['info'];
+handler.command = ['ping'];
+handler.register = true;
+
+export default handler;
