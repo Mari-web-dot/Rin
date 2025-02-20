7d9267f31d79f6097a2f189cd6ccc087a7bc013b
Mari-web-dot <lachico099@gmail.com>
2025-02-20 12:26:03 -0600

    Update main-allfake.js

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -64,7 +64,7 @@ global.emoji = '🍓'
 global.emoji2 = '🍓'
 global.emoji3 = '🧣'
 global.emoji4 = ''🧣
-global.emoji5 = '🎒'
+global.emoji5 = '💉'
 global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
 
 //mensaje en espera
